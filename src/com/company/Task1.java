4a76307b97f638d0056ede1d30e51b4491f9c982
Irena Vakulenko <ivvvv@ukr.net>
2021-05-23 13:55:45 +0300

    работа над ошибками

--- src/com/company/Task1.java
+++ src/com/company/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("HelloWorld");
             } else if (i % 3 == 0) {
                 System.out.print("Hello");
--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println(createText());
+        System.out.println(createStringWithNumbers());
     }
 
-    private static String createText() {
+    private static String createStringWithNumbers() {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 1; i <= 30; i++) {
             stringBuilder.append("(").
